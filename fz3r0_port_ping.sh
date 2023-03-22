ffab567131557c19f3bda3dc3b53c8e4bc1ff9a4
Fz3r0 <94720207+Fz3r0@users.noreply.github.com>
2023-03-22 16:36:57 -0500

    Update fz3r0_port_ping.sh

--- fz3r0_port_ping.sh
+++ fz3r0_port_ping.sh
@@ -1,32 +1,16 @@
-#!/bin/bash
+# Solicitar la dirección IP y el puerto al usuario
+echo "Ingresa la dirección IP:"
+read ip_address
+echo "Ingresa el número de puerto:"
+read port
 
-while getopts ":ip:po:" opt; do
-  case $opt in
-    ip)
-      ip_address=$OPTARG
-      ;;
-    po)
-      port=$OPTARG
-      ;;
-    \?)
-      echo "Opción inválida: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Opción -$OPTARG requiere un argumento." >&2
-      exit 1
-      ;;
-  esac
-done
-
+# Verificar que se hayan proporcionado valores válidos para la dirección IP y el puerto
 if [[ -z "$ip_address" || -z "$port" ]]; then
-  echo "Se requiere especificar la dirección IP y el puerto. Uso: fz3r0_port_ping.sh -ip [IP ADDRESS] -po [PORT]"
+  echo "Se requiere especificar la dirección IP y el puerto."
   exit 1
 fi
 
-echo "Presione cualquier tecla para iniciar el ping infinito a $ip_address:$port"
-read -n1 -r
-
+# Iniciar el bucle infinito de ping
 while true; do
   echo -n "Pinging $ip_address:$port... "
   nc -vz $ip_address $port && echo "SUCCESS" || echo "FAILED"
